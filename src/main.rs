e49896a2b9b7d39ce2713228cd836cf4ab14c494
dskleingeld <dskleingeldgmail.com>
2020-02-14 00:41:29 +0100

    general layout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+mod certificate_manager;
+use serde::{Deserialize};
+
+// list of (dirs to copy keys to
+// systemd service names)
+#[derive(Debug, Deserialize)]
+struct Config {
+    domain: String,
+
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    //parse config file
+
+    loop{
+        //count valid days left
+        //to duration (clip 0-...)
+
+        //wait for duration
+        //update cert
+        //distribute cert
+
+        //restart (systemd) services
+        //TODO use std::command
+    }
 }
